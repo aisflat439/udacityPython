0fa0325e5c4101e202afe31e9acd67b0027b517c
Devin P <devinfitzsimons@yahoo.com>
2015-02-12 17:19:43 -0500

    Consolidated Udacity into on folder

---
+++ turtles/drawsquare.py
@@ -0,0 +1,21 @@
+import turtle
+
+distance = 100
+
+def draw_square():
+	window = turtle.Screen()
+	window.bgcolor("red")
+
+	devin = turtle.Turtle()
+	devin.shape("turtle")
+	devin.shapesize(3,3,5)
+	devin.color("blue")
+	devin.speed(1)
+
+	for x in range(0,4):
+		devin.forward(distance)
+		devin.right(90)
+
+	window.exitonclick()
+
+draw_square()
