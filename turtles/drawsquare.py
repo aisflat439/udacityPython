ca9d0d4bc4baaccbd301cbce706f471676d7b12f
Devin P <devinfitzsimons@yahoo.com>
2015-02-13 17:36:37 -0500

    add circle and reformat some

--- turtles/drawsquare.py
+++ turtles/drawsquare.py
@@ -1,11 +1,10 @@
 import turtle
 
 distance = 100
+degrees = 90
+pen_size = 10
 
 def draw_square():
-	window = turtle.Screen()
-	window.bgcolor("red")
-
 	devin = turtle.Turtle()
 	devin.shape("turtle")
 	devin.shapesize(3,3,5)
@@ -14,14 +13,20 @@ def draw_square():
 
 	for x in range(0,4):
 		devin.forward(distance)
-		devin.right(90)
+		devin.right(degrees)
 
+
+def draw_circle():
 	sarah = turtle.Turtle()
 	sarah.shape("arrow")
 	sarah.color("green")
-	sarah.pensize(10)
-	sarah.circle(100)
+	sarah.pensize(pen_size)
+	sarah.circle(distance)
 
-	window.exitonclick()
+window = turtle.Screen()
+window.bgcolor("red")
 
 draw_square()
+draw_circle()
+
+window.exitonclick()
