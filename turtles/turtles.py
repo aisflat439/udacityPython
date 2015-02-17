943e72a05ea890313520ae19c33ef4f09c73c0ea
Devin P <devinfitzsimons@yahoo.com>
2015-02-16 19:13:41 -0500

    set window

--- turtles/turtles.py
+++ turtles/turtles.py
@@ -1,1 +1,19 @@
 import turtle
+
+def draw_square(t):
+	for x in range(0,4):
+		t.forward(s_distance)
+		t.right(s_degrees)
+
+def draw_circle(t):
+	t.circle(c_distance)
+
+def draw_triangle(t):
+	for x in range(0,3):
+		t.forward(t_distance)
+		t.left(tri_degrees)
+
+window = turtle.Screen()
+window.bgcolor("red")
+
+window.exitonclick()
