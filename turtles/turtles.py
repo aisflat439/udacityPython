0d9b1f47ce9d5ad2ccc40794bbac3b25bb58ee18
Devin P <devinfitzsimons@yahoo.com>
2015-02-16 19:32:34 -0500

    built turles

--- turtles/turtles.py
+++ turtles/turtles.py
@@ -1,5 +1,11 @@
 import turtle
 
+s_distance = 100
+s_degrees = 90
+t_distance = 200
+tri_degrees = 120
+c_distance = 150
+
 def draw_square(t):
 	for x in range(0,4):
 		t.forward(s_distance)
@@ -14,6 +20,19 @@ def draw_triangle(t):
 		t.left(tri_degrees)
 
 window = turtle.Screen()
-window.bgcolor("red")
+window.setup(900,900)
+window.bgcolor("blue")
+
+redTurtle = turtle.Turtle()
+greenTurtle = turtle.Turtle()
+whiteTurtle = turtle.Turtle()
+
+redTurtle.color("red")
+greenTurtle.color("green")
+whiteTurtle.color("white")
+
+draw_square(whiteTurtle)
+draw_circle(redTurtle)
+draw_triangle(greenTurtle )
 
 window.exitonclick()
