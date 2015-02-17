1f95b7ea731c63172f39d1d4c2642f627058bef1
Devin P <devinfitzsimons@yahoo.com>
2015-02-17 14:11:23 -0500

    made a flower in a loop. it's weird

--- turtles/turtles.py
+++ turtles/turtles.py
@@ -5,6 +5,10 @@ s_degrees = 90
 t_distance = 200
 tri_degrees = 120
 c_distance = 150
+gx=0
+gy=0
+btx = 0
+bty = 0
 
 def draw_square(t):
 	for x in range(0,4):
@@ -19,38 +23,80 @@ def draw_triangle(t):
 		t.forward(t_distance)
 		t.left(tri_degrees)
 
-def moving_circle(t):
-	x = 0
-	y = 0
-	for x in range(0,50):
-		draw_circle(t)
-		t.penup()
-		t.goto(x,y)
-		t.pendown()
-		if y <= 90:
-			x = x + 10
-		elif y < (-90):
-			x = x - 10
-		if x <= 90:
-			y = y + 10
-		elif x < (-90):
-			y = y - 10
+def move_turtle_up(t):
+	global gy
+	t.penup()
+	t.goto(gx,gy)
+	t.pendown()
+	gy+=10
+
+def move_turtle_down(t):
+	global gy
+	t.penup()
+	t.goto(gx,gy)
+	t.pendown()
+	gy-=10
+
+def move_turtle_right(t):
+	global gx
+	t.penup()
+	t.goto(gx,gy)
+	t.pendown()
+	gx+=10
+
+def move_turtle_left(t):
+	global gx
+	t.penup()
+	t.goto(gx,gy)
+	t.pendown()
+	gx-=10
 
 window = turtle.Screen()
 window.setup(900,900)
 window.bgcolor("blue")
 
-redTurtle = turtle.Turtle()
-greenTurtle = turtle.Turtle()
-whiteTurtle = turtle.Turtle()
-
-whiteTurtle.speed(0)
-
+myTurtle = turtle.Turtle()
+blackTurtle = turtle.Turtle()
 redTurtle.color("red")
 greenTurtle.color("green")
 whiteTurtle.color("white")
+yellowTurtle.color("yellow")
+blackTurtle.color("black")
+whiteTurtle.speed(0)
+redTurtle.speed(0)
+greenTurtle.speed(0)
+yellowTurtle.speed(0)
+blackTurtle.speed(0)
+blackTurtle.pensize(15)
 
-draw_circle(redTurtle)
-moving_circle(whiteTurtle)
+for y in range(0,2):
+	gx+=1
+	gy+=1
+	for x in range(0,10):
+		draw_circle(whiteTurtle)
+		move_turtle_right(whiteTurtle)
+		move_turtle_up(whiteTurtle)
+		# btx = whiteTurtle.xcor()
+		# bty = wh
+	for x in range(0,10):
+		draw_circle(redTurtle)
+		move_turtle_down(redTurtle)
+		move_turtle_right(redTurtle)
+	for x in range(0,10):
+		draw_circle(greenTurtle)
+		move_turtle_down(greenTurtle)
+		move_turtle_left(greenTurtle)
+	for x in range(0,10):
+		draw_circle(yellowTurtle)
+		move_turtle_up(yellowTurtle)
+		move_turtle_left(yellowTurtle)
+
+blackTurtle.penup()
+btx = whiteTurtle.xcor()
+bty = whiteTurtle.ycor()
+blackTurtle.goto(btx, bty)
+blackTurtle.pendown()
+gy = -500
+blackTurtle.goto(btx, gy)
 
 window.exitonclick()
