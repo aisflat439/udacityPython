b279756b911328369d3b4280172419130316d658
Devin P <devinfitzsimons@yahoo.com>
2015-02-18 15:43:21 -0500

    wth

--- turtles/turtles.py
+++ turtles/turtles.py
@@ -2,7 +2,7 @@ import turtle
 
 s_distance = 100
 s_degrees = 90
-t_distance = 200
+t_distance = 100
 tri_degrees = 120
 c_distance = 150
 gx=0
@@ -18,10 +18,15 @@ def draw_square(t):
 def draw_circle(t):
 	t.circle(c_distance)
 
-def draw_triangle(t):
+def draw_right_leaf(t):
 	for x in range(0,3):
 		t.forward(t_distance)
 		t.left(tri_degrees)
+
+def draw_left_leaf(t):
+	for x in range(0,3):
+		t.backward(t_distance)
+		t.right(tri_degrees)
 
 def move_turtle_up(t):
 	global gy
@@ -56,7 +61,7 @@ def draw_stem(t):
 	myTurtle.goto(btx, bty)
 	myTurtle.pendown()
 	myTurtle.pensize(8)
-	myTurtle.goto(btx,-250)
+	myTurtle.goto(btx,-350)
 
 window = turtle.Screen()
 window.setup(900,900)
@@ -65,7 +70,7 @@ window.bgcolor("blue")
 myTurtle = turtle.Turtle()
 myTurtle.speed(0)
 
-for y in range(0,2):
+for y in range(0,10):
 	gx+=1
 	gy+=1
 	for x in range(0,10):
@@ -92,5 +97,8 @@ for y in range(0,2):
 		move_turtle_left(myTurtle)
 
 draw_stem(myTurtle)
+myTurtle.color("green")
+draw_right_leaf(myTurtle)
+draw_left_leaf(myTurtle)
 
 window.exitonclick()
