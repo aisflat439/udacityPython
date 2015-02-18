3c275e4f9a5f602532024f8e8fd004422d52f373
Devin P <devinfitzsimons@yahoo.com>
2015-02-18 15:38:22 -0500

    profanity editor finished.

--- texts/movie_quotes.py
+++ texts/movie_quotes.py
@@ -4,10 +4,24 @@
 # if it has a curse then print(has a curse)
 # else print("message")
 
+import urllib
+
 def read_text():
-	quotes = open("/home/devindesktop/Udacity/texts/quotes.txt")
+	quotes = open("/home/devindesktop/tmp/tswift.txt")
 	contents_of_file = quotes.read()
 	print(contents_of_file)
 	quotes.close()
+	check_profanity(contents_of_file)
+
+def check_profanity(text):
+	connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text)
+	output = connection.read()
+	connection.close()
+	if "true" in output:
+		print("Profanity alert!")
+	elif "false" in output:
+		print("this document has no curses")
+	else:
+		print("document read failure")
 
 read_text()
