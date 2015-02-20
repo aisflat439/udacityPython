5577f64c046b08ec9558aa241d5485c8afc4ad0b
Devin P <devinfitzsimons@yahoo.com>
2015-02-20 03:51:18 -0500

    add avatar

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,8 +1,14 @@
 import media
 
 toy_story = media.Movie("Toy Story",
-	"about a boy and toys.",
+	"About a boy and toys.",
 	"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
 	"https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
+avatar = media.Movie("Avatar",
+	"A marine on an alien planet.",
+	"http://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+	"https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
 print(toy_story.storyline)
+print(avatar.storyline)
